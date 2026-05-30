59050c586234b0103a0aa533b95ef5fb779a98a5
darshanshinde1 <darshanshinde0809@gmail.com>
2026-05-30 12:56:05 +0530

    Added Coffee Machine OOP project

---
+++ oop-projects/coffee-machine-oop/main.py
@@ -0,0 +1,26 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+machine_on = True
+while machine_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ")
+
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        machine_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            payment = money_machine.make_payment(drink.cost)
+            if payment:
+                coffee_maker.make_coffee(drink)
+
+
